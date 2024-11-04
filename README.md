c18d030cb89cd68e410828c178dd14eb32efe785
saqib-rt <85487989+saqib-rt@users.noreply.github.com>
2024-11-04 21:58:18 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,117 @@
+<div align="right">
+
+</div>
+
+# <p align="center">Agent to review code in Github PRs</p>
+
+<p align="center">
+  <a href="https://discord.gg/ccZn9ZMfFf">
+    <img src="https://img.shields.io/badge/chat-Discord-7289DA?logo=discord" alt="flows.network Discord">
+  </a>
+  <a href="https://twitter.com/flows_network">
+    <img src="https://img.shields.io/badge/Twitter-1DA1F2?logo=twitter&amp;logoColor=white" alt="flows.network Twitter">
+  </a>
+   <a href="https://flows.network/flow/createByTemplate/code-review-for-github-pull-requests">
+    <img src="https://img.shields.io/website?up_message=deploy&url=https%3A%2F%2Fflows.network%2Fflow%2Fnew" alt="Create a flow">
+  </a>
+</p>
+
+[Deploy this function on flows.network](https://flows.network/flow/createByTemplate/github-pr-review-llm), and you will get an AI agent to review changed source code files in GitHub Pull Requests. It helps busy open source contributors understand and make decisions on PRs faster! Here are some examples. Notice how the code review bot provides code snippets to show you how to improve the code!
+
+> We recommend you to use a [Gaia node](https://github.com/GaiaNet-AI/gaianet-node) running an open source coding LLM as the backend to perform PR reviews and summarizations. You can use [a community node](https://docs.gaianet.ai/user-guide/nodes#codestral) or run a node [on your own computer](https://github.com/GaiaNet-AI/node-configs/tree/main/codestral-0.1-22b)!
+
+* [[C++] Improve the WasmEdge C++ SDK](https://github.com/WasmEdge/WasmEdge/pull/2428#issuecomment-1524733889)
+* [[C++] Create an OpenCV plugin for WasmEdge](https://github.com/WasmEdge/WasmEdge/pull/2403#issuecomment-1509595889)
+* [[Haskell] Improve WasmEdge Component Model tooling](https://github.com/second-state/witc/pull/73#issuecomment-1509586233)
+
+This bot reviews **changed files in the PR**. Alternatively, you can use [this bot](https://github.com/flows-network/github-pr-summary) to summarize commits in the PR.
+
+## How it works
+
+This flow function is triggered when a new PR is raised in the designated GitHub repo. The flow function collects the changed files in the PR, and asks ChatGPT/4 to review and summarize it. The result is then posted back to the PR as a comment. The flow functions are written in Rust and run in hosted [WasmEdge Runtimes](https://github.com/wasmedge) on [flows.network](https://flows.network/).
+
+* The code review comment is updated automatically every time a new commit is pushed to this PR.
+* A new code review could be triggered when someone says a magic *trigger phrase* in the PR's comments section. The default trigger phrase is "flows review".
+
+## Deploy your own code review bot in 3 simple steps
+
+1. Create a bot from template
+2. Connect to an LLM
+3. Connect to GitHub for access to the target repo
+
+### 0 Prerequisites
+
+You will also need to sign into [flows.network](https://flows.network/) from your GitHub account. It is free.
+
+### 1 Create a bot from template
+
+Create a flow function from [this template](https://flows.network/flow/createByTemplate/github-pr-review-llm).
+It will fork a repo into your personal GitHub account. Your flow function will be compiled from the source code
+in your forked repo. You can configure how it is summoned from the GitHub PR.
+
+* `trigger_phrase` : The magic words to write in a PR comment to summon the bot. It defaults to "flows review".
+
+Click on the **Create and Build** button.
+
+> Alternatively, fork this repo to your own GitHub account. Then, from [flows.network](https://flows.network/), you can **Create a Flow** and select your forked repo. It will create a flow function based on the code in your forked repo. Click on the **Advanced** button to see configuration options for the flow function.
+
+[<img width="450" src="create.png">](create.png)
+
+### 2 Connect to an LLM
+
+Configure the LLM API service you want to use to review the PRs.
+
+* `llm_api_endpoint` : The OpenAI compatible API service endpoint for the LLM to conduct code reviews. We recommend the [Codestral Gaia node](https://github.com/GaiaNet-AI/node-configs/tree/main/codestral-0.1-22b): `https://codestral.us.gaianet.network/v1`
+* `llm_model_name` : The model name required by the API service. We recommend the following model name for the above public Gaia node: `codestral`
+* `llm_ctx_size` : The context window size of the selected model. The Codestral model has a 32k context window, which is `32768`.
+* `llm_api_key` : Optional: The API key if required by the LLM service provider. It is not required for the Gaia node.
+
+Click on the **Continue** button.
+
+### 3 Connect to GitHub for access to the target repo
+
+Next, you will tell the bot which GitHub repo it needs to monitor for upcoming PRs to review.
+
+* `github_owner`: GitHub org for the repo you want to review PRs
+* `github_repo` : GitHub repo you want to review PRs
+
+> Let's see an example. You would like to deploy the bot to review code in PRs on `WasmEdge/wasmedge_hyper_demo` repo. Here `github_owner = WasmEdge` and `github_repo = wasmedge_hyper_demo`.
+
+Finally, the GitHub repo will need to give you access so that the flow function can access and review its PRs!
+Click on the **Connect** or **+ Add new authentication** button to give the function access to the GitHub repo. You'll be redirected to a new page where you must grant flows.network permission to the repo.
+
+[<img width="450" src="llm.png">](llm.png)
+
+Click on **Deploy**.
+
+### Wait for the magic!
+
+This is it! You are now on the flow details page waiting for the flow function to build. As soon as the flow's status became `running`, the bot is ready to give code reviews! The bot is summoned by every new PR, every new commit, as well as magic words (i.e., `trigger_phrase`) in PR comments.
+
+[<img width="450" src="target.png">](target.png)
+
+## FAQ
+
+### Customize the bot
+
+The bot's source code is available in the GitHub repo you cloned from the template. Feel free to make changes to the source code (e.g., model, context length, API key and prompts) to fit your own needs. If you need help, [ask in Discord](https://discord.gg/ccZn9ZMfFf)!
+
+### Use the bot on multiple repos
+
+You can [mannually create a new flow](https://flows.network/flow/new) and import the source code repo for the bot (i.e., the repo you cloned from the template). Then, you can use the flow config to specify the `github_owner` and `github_repo` to point to the target repo you need to deploy the bot on. Deploy and authorize access to that target repo.
+
+You can repeat this for all target repos you would like to deploy this bot on.
+
+> You could have a single flow function repo deployed as the source code for multiple bots. When you update the source code in the repo, and push it to GitHub, it will change the behavior of all the bots.
+
+### Change the magic phrase
+
+Go to the "Settings" tab of the running flow function for the bot, you can update the `trigger_phrase` config. The value of this config is the magic phrase the user will say to trigger a review from a PR comment.
+
+## Credits
+
+This flow function is originally created by [Jay Chen](https://github.com/jaykchen), and [jinser](https://github.com/jetjinser) made significant contributions to optimize the event triggers from GitHub.
+
+<p align="center">
+<a href="https://www.producthunt.com/posts/gpt-nitro-for-github-pr?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-gpt&#0045;nitro&#0045;for&#0045;github&#0045;pr" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=387993&theme=light" alt="GPT&#0032;Nitro&#0032;for&#0032;Github&#0032;PR - A&#0032;ChatGPT&#0045;based&#0032;reviewer&#0032;for&#0032;your&#0032;GitHub&#0032;pull&#0032;requests | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+</p>
---
+++ src/github-pr-review.rs
@@ -0,0 +1,248 @@
+use dotenv::dotenv;
+use flowsnet_platform_sdk::logger;
+use github_flows::{
+    event_handler, get_octo, listen_to_event,
+    octocrab::models::CommentId,
+    octocrab::models::webhook_events::{WebhookEvent, WebhookEventPayload},
+    octocrab::models::webhook_events::payload::{IssueCommentWebhookEventAction, PullRequestWebhookEventAction},
+    GithubLogin,
+};
+use llmservice_flows::{
+    chat::{ChatOptions},
+    LLMServiceFlows,
+};
+use std::env;
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn on_deploy() {
+    dotenv().ok();
+    logger::init();
+    log::debug!("Running github-pr-review/main");
+
+    let owner = env::var("github_owner").unwrap_or("juntao".to_string());
+    let repo = env::var("github_repo").unwrap_or("test".to_string());
+
+    listen_to_event(&GithubLogin::Default, &owner, &repo, vec!["pull_request", "issue_comment"]).await;
+}
+
+#[event_handler]
+async fn handler(event: Result<WebhookEvent, serde_json::Error>) {
+    dotenv().ok();
+    logger::init();
+    log::debug!("Running github-pr-review/main handler()");
+
+    let owner = env::var("github_owner").unwrap_or("juntao".to_string());
+    let repo = env::var("github_repo").unwrap_or("test".to_string());
+    let trigger_phrase = env::var("trigger_phrase").unwrap_or("flows review".to_string());
+    let llm_api_endpoint = env::var("llm_api_endpoint").unwrap_or("https://api.openai.com/v1".to_string());
+    let llm_model_name = env::var("llm_model_name").unwrap_or("gpt-4o".to_string());
+    let llm_ctx_size = env::var("llm_ctx_size").unwrap_or("16384".to_string()).parse::<u32>().unwrap_or(0);
+    let llm_api_key = env::var("llm_api_key").unwrap_or("LLAMAEDGE".to_string());
+
+    //  The soft character limit of the input context size
+    //  This is measured in chars. We set it to be 2x llm_ctx_size, which is measured in tokens.
+    let ctx_size_char : usize = (2 * llm_ctx_size).try_into().unwrap_or(0);
+
+    let payload = event.unwrap();
+    let mut new_commit : bool = false;
+    let (title, pull_number, _contributor) = match payload.specific {
+        WebhookEventPayload::PullRequest(e) => {
+            if e.action == PullRequestWebhookEventAction::Opened {
+                log::debug!("Received payload: PR Opened");
+            } else if e.action == PullRequestWebhookEventAction::Synchronize {
+                new_commit = true;
+                log::debug!("Received payload: PR Synced");
+            } else {
+                log::debug!("Not a Opened or Synchronize event for PR");
+                return;
+            }
+            let p = e.pull_request;
+            (
+                p.title.unwrap_or("".to_string()),
+                p.number,
+                p.user.unwrap().login,
+            )
+        }
+        WebhookEventPayload::IssueComment(e) => {
+            if e.action == IssueCommentWebhookEventAction::Deleted {
+                log::debug!("Deleted issue comment");
+                return;
+            }
+            log::debug!("Other event for issue comment");
+
+            let body = e.comment.body.unwrap_or_default();
+
+            // if e.comment.performed_via_github_app.is_some() {
+            //     return;
+            // }
+            // TODO: Makeshift but operational
+            if body.starts_with("Hello, I am a [code review agent]") {
+                log::info!("Ignore comment via agent");
+                return;
+            };
+
+            if !body.to_lowercase().starts_with(&trigger_phrase.to_lowercase()) {
+                log::info!("Ignore the comment without magic words");
+                return;
+            }
+
+            (e.issue.title, e.issue.number, e.issue.user.login)
+        }
+        _ => return,
+    };
+
+    let chat_id = format!("PR#{pull_number}");
+    let system = &format!("You are an experienced software developer. You will review a source code file and its patch related to the subject of \"{}\". Please be as concise as possible while being accurate.", title);
+    let mut lf = LLMServiceFlows::new(&llm_api_endpoint);
+    lf.set_api_key(&llm_api_key);
+
+    let octo = get_octo(&GithubLogin::Default);
+    let issues = octo.issues(owner.clone(), repo.clone());
+    let mut comment_id: CommentId = 0u64.into();
+    if new_commit {
+        // Find the first "Hello, I am a [code review agent]" comment to update
+        match issues.list_comments(pull_number).send().await {
+            Ok(comments) => {
+                for c in comments.items {
+                    if c.body.unwrap_or_default().starts_with("Hello, I am a [code review agent]") {
+                        comment_id = c.id;
+                        break;
+                    }
+                }
+            }
+            Err(error) => {
+                log::error!("Error getting comments: {}", error);
+                return;
+            }
+        }
+    } else {
+        // PR OPEN or Trigger phrase: create a new comment
+        match issues.create_comment(pull_number, "Hello, I am a [code review agent](https://github.com/flows-network/github-pr-review/) on [flows.network](https://flows.network/).\n\nIt could take a few minutes for me to analyze this PR. Relax, grab a cup of coffee and check back later. Thanks!").await {
+            Ok(comment) => {
+                comment_id = comment.id;
+            }
+            Err(error) => {
+                log::error!("Error posting comment: {}", error);
+                return;
+            }
+        }
+    }
+    if comment_id == 0u64.into() { return; }
+
+    let pulls = octo.pulls(owner.clone(), repo.clone());
+    let mut resp = String::new();
+    resp.push_str("Hello, I am a [code review agent](https://github.com/flows-network/github-pr-review/) on [flows.network](https://flows.network/). Here are my reviews of changed source code files in this PR.\n\n------\n\n");
+    match pulls.list_files(pull_number).await {
+        Ok(files) => {
+            // let client = reqwest::Client::new();
+            for f in files.items {
+                let filename = &f.filename;
+                if filename.ends_with(".md") || filename.ends_with(".js") || filename.ends_with(".css") || filename.ends_with(".html") || filename.ends_with(".htm") {
+                    continue;
+                }
+
+                // The f.raw_url is a redirect. So, we need to construct our own here.
+                let contents_url = f.contents_url.as_str();
+                if contents_url.len() < 40 { continue; }
+                let hash = &contents_url[(contents_url.len() - 40)..];
+                let raw_url = format!(
+                    "https://raw.githubusercontent.com/{owner}/{repo}/{}/{}", hash, filename
+                );
+
+                log::debug!("Fetching url: {}", raw_url);
+                let res = match reqwest::get(raw_url.as_str()).await {
+                    Ok(r) => r,
+                    Err(e) => {
+                        log::error!("Error fetching file {}: {}", filename, e);
+                        continue;
+                    }
+                };
+                // let res = reqwest::get(raw_url.as_str()).await.unwrap();
+                // let res = client.get(raw_url.as_str()).send().await.unwrap();
+                log::debug!("Fetched file: {}", filename);
+                let file_as_text = res.text().await.unwrap();
+                let t_file_as_text = truncate(&file_as_text, ctx_size_char);
+
+                resp.push_str("## [");
+                resp.push_str(filename);
+                resp.push_str("](");
+                resp.push_str(f.blob_url.as_str());
+                resp.push_str(")\n\n");
+
+                log::debug!("Sending file to LLM: {}", filename);
+                let co = ChatOptions {
+                    model: Some(&llm_model_name),
+                    token_limit: llm_ctx_size,
+                    restart: true,
+                    system_prompt: Some(system),
+                    ..Default::default()
+                };
+                let question = "Review the following source code and report only major bugs or issues. The most important coding issues should be reported first. You should report NO MORE THAN 3 issues. Be very concise and explain each coding issue in one sentence. The code might be truncated. NEVER comment on the completeness of the source code.\n\n".to_string() + t_file_as_text;
+                match lf.chat_completion(&chat_id, &question, &co).await {
+                    Ok(r) => {
+                        resp.push_str("#### Potential issues");
+                        resp.push_str("\n\n");
+                        resp.push_str(&r.choice);
+                        resp.push_str("\n\n");
+                        log::debug!("Received LLM resp for file: {}", filename);
+                    }
+                    Err(e) => {
+                        resp.push_str("#### Potential issues");
+                        resp.push_str("\n\n");
+                        resp.push_str("N/A");
+                        resp.push_str("\n\n");
+                        log::error!("LLM returns error for file review for {}: {}", filename, e);
+                    }
+                }
+
+                log::debug!("Sending patch to LLM: {}", filename);
+                let co = ChatOptions {
+                    model: Some(&llm_model_name),
+                    token_limit: llm_ctx_size,
+                    restart: true,
+                    system_prompt: Some(system),
+                    ..Default::default()
+                };
+                let patch_as_text = f.patch.unwrap_or("".to_string());
+                let t_patch_as_text = truncate(&patch_as_text, ctx_size_char);
+                let question = "The following is a change patch for the file. Please summarize key changes in short bullet points. List the most important changes first. You list should contain no more than the top 3 most important changes.  \n\n".to_string() + t_patch_as_text;
+                match lf.chat_completion(&chat_id, &question, &co).await {
+                    Ok(r) => {
+                        resp.push_str("#### Summary of changes");
+                        resp.push_str("\n\n");
+                        resp.push_str(&r.choice);
+                        resp.push_str("\n\n");
+                        log::debug!("Received LLM resp for patch: {}", filename);
+                    }
+                    Err(e) => {
+                        resp.push_str("#### Summary of changes");
+                        resp.push_str("\n\n");
+                        resp.push_str("N/A");
+                        resp.push_str("\n\n");
+                        log::error!("LLM returns error for patch review for {}: {}", filename, e);
+                    }
+                }
+            }
+        },
+        Err(_error) => {
+            log::error!("Cannot get file list");
+        }
+    }
+
+    // Send the entire response to GitHub PR
+    // issues.create_comment(pull_number, resp).await.unwrap();
+    match issues.update_comment(comment_id, resp).await {
+        Err(error) => {
+            log::error!("Error posting resp: {}", error);
+        }
+        _ => {}
+    }
+}
+
+fn truncate(s: &str, max_chars: usize) -> &str {
+    match s.char_indices().nth(max_chars) {
+        None => s,
+        Some((idx, _)) => &s[..idx],
+    }
+}
